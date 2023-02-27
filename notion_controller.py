6e86d9560ccb352147c91e46942a3ec156ae9477
idrisimo <idrissilva@hotmail.com>
2023-02-27 23:29:01 +0000

    turned functions into class

--- notion_controller.py
+++ notion_controller.py
@@ -31,39 +31,48 @@ for page in my_pages["results"]:
   # update = notion.pages.update(**{"page_id":page_id, "properties":{"Instagram Status": {'select': {'color': 'green', 'id': 'Dkrp', 'name': 'Published'}}}})
 
 
+class Notion_controller():
 
-
-def get_database_id(database_name):
-  params = {"query": database_name, "filter": {"value": 'database',"property": 'object' }}
-  database = notion.search(**params)
-  try:
-    database_id = database["results"][0]["id"]
-    return database_id
-  except Exception as err:
-    print(f"Something went wrong:\n{err}")
-  
-def get_unpublished_pages(database_id):
-  params = {"database_id": database_id, "filter": {"property":"Instagram Status", "select": {"is_empty":True}}}
-  all_pages_response = notion.databases.query(**params)
-  return all_pages_response
-
-def page_info_extractor(pages):
-  pages_list =[]
-  for page in pages["results"]:
-    title = page["properties"]["Content"]["title"][0]["text"]["content"]
-    page_id = page["id"]
+  def __init__(self, auth_token):
+    self.auth_token = auth_token
+    
+  def initialise(self):
     try:
-      date = page["properties"]["Date"]["date"]["start"]
-    except TypeError as err:
-      print("No date")
-      date = "No Date available"
-    insta_status = page["properties"]["Instagram Status"]["select"]
-    pages_list.append({"page id": page_id,"title": title, "date": date, "instagram status": insta_status})
-  return pages_list
+      self.notion = Client(auth=self.auth_token)
+    except Exception as err:
+      print(f"Something went wrong:\n {err}")
+      
+  def get_database_id(self, database_name):
+    params = {"query": database_name, "filter": {"value": 'database',"property": 'object' }}
+    database = self.notion.search(**params)
+    try:
+      database_id = database["results"][0]["id"]
+      return database_id
+    except Exception as err:
+      print(f"Something went wrong:\n{err}")
+    
+  def get_unpublished_pages(self, database_id):
+    params = {"database_id": database_id, "filter": {"property":"Instagram Status", "select": {"is_empty":True}}}
+    all_pages_response = self.notion.databases.query(**params)
+    return all_pages_response
 
-def update_page_properties(page_id, properties):
-  params = {"page_id":page_id, "properties":properties}
-  update_response = notion.pages.update(**params)
-  return update_response
+  def page_info_extractor(self, pages):
+    pages_list =[]
+    for page in pages["results"]:
+      title = page["properties"]["Content"]["title"][0]["text"]["content"]
+      page_id = page["id"]
+      try:
+        date = page["properties"]["Date"]["date"]["start"]
+      except TypeError as err:
+        print("No date")
+        date = "No Date available"
+      insta_status = page["properties"]["Instagram Status"]["select"]
+      pages_list.append({"page id": page_id,"title": title, "date": date, "instagram status": insta_status})
+    return pages_list
+
+  def update_page_properties(self, page_id, properties):
+    params = {"page_id":page_id, "properties":properties}
+    update_response = self.notion.pages.update(**params)
+    return update_response
 
 
